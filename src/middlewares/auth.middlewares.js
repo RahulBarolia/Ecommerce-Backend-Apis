e7af3c3b6c21b26ebfaa96a1868bf971ab5f3633
Rahul Barolia <rahulbaroliar89@gmail.com>
2025-03-30 22:05:59 +0530

    second-commit

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -22,7 +22,7 @@ export const protectRoute = async (req, res, next) => {
     const user = await User.findById(decoded.userId).select("-password");
 
     if (!user) {
-      return res.staus(400).json({ message: "User not found" });
+      return res.status(400).json({ message: "User not found" });
     }
 
     req.user = user;
